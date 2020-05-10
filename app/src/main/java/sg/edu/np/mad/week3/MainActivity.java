7135dc75bf747049ee6d9fa6e0a6e0ab30ef2b01
BuildTools <unconfigured@null.spigotmc.org>
2020-05-10 20:49:44 +0800

    practical done

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,11 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer audioBaby = MediaPlayer.create(this, R.raw.baby_laughing);
+
+        VideoView rabbidVideo = findViewById(R.id.rabbidVideo);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid)
+        );
+        rabbidVideo.start();
     }
 }
